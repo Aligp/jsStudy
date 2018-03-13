7b9fb6762df10a4ca638cf1958396b13cee4defc
BaiDuMes <BaiDuMes@163.com>
2018-03-13 11:17:37 +0800

    add quickSort

--- gp.fn.js
+++ gp.fn.js
@@ -124,3 +124,40 @@ function deepCopy2(obj) {
     }
     return newObj;
 }
+
+/**
+ * 给定的数组排序
+ * @param arr 给定的数组
+ * @param boolean true为正序，false为倒序
+ * @returns {*}
+ */
+function quickSort(arr, boolean) {
+    if (arr.length <= 1) {
+        return arr;
+    }
+    //找到中间的数
+    var num = Math.floor(arr.length / 2);
+    var numVal = arr.splice(num, 1);
+
+    var left = [];
+    var right = [];
+
+    for (var i = 0; i < arr.length; i++) {
+        if (boolean) {
+            if (arr[i] < numVal) {
+                left.push(arr[i]);
+            } else {
+                right.push(arr[i]);
+            }
+        } else {
+            if (arr[i] < numVal) {
+                right.push(arr[i]);
+            } else {
+                left.push(arr[i]);
+            }
+        }
+    }
+
+    return quickSort(left, boolean).concat([numVal], quickSort(right, boolean));
+
+}
